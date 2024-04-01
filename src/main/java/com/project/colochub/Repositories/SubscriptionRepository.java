7bd9dc7f49c52d7853f50735f38d68c0e4fafde3
imads <imadeddinzaoui@gmail.com>
2024-04-01 20:26:23 +0000

    Small refactoring in dev process

--- src/main/java/com/project/colochub/Repositories/SubscriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.colochub.Repositories;
-
-import com.project.colochub.Models.Entities.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
-}
--- src/main/java/com/project/colochub/Services/SubscriptionService.java
+++
@@ -1,7 +0,0 @@
-package com.project.colochub.Services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class SubscriptionService {
-}
